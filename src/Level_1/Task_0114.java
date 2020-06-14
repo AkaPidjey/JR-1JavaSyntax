60ca7ded65e63965625468eba3a3e8a55de55e8d
AkaPidjey <pidjey@gmail.com>
2020-06-14 22:44:09 +0300

    * Title - JavaRush. Learn once - use anywhere # 0114

---
+++ src/Level_1/Task_0114.java
@@ -0,0 +1,28 @@
+package Level_1;
+
+/**
+ * Title - JavaRush. Learn once - use anywhere # 0114
+ * @task Напиши программу, которая выводит на экран надпись: "JavaRush. Learn once - use anywhere" 10 раз.
+ *
+ * Требования:
+ * •	Программа должна выводить текст.
+ * •	Текст должен начинаться с "JavaRush".
+ * •	Текст должен заканчиваться на "use anywhere".
+ * •	Текст должен состоять из 10 строк.
+ * •	Выводимый текст должен соответствовать заданию.
+ */
+
+public class Task_0114 {
+    public static void main(String[] args) {
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+    }
+}
