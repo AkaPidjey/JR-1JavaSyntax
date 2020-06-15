3d023134c03b269a0721a8a0cade8928eac08646
AkaPidjey <pidjey@gmail.com>
2020-06-15 17:14:32 +0300

    * Title - Великолепная восьмерка # 0212.

---
+++ src/Level_2/Task_0212.java
@@ -0,0 +1,32 @@
+package Level_2;
+
+/**
+ * Title - Великолепная восьмерка # 0212.
+ * @task Создай 10 переменных типа Cat и 8 объектов типа Cat.
+ *
+ * Требования:
+ * •	Программа не должна выводить текст на экран.
+ * •	В методе main должно быть 10 переменных типа Cat.
+ * •	В методе main 8 переменных должны быть проинициализированы сразу при создании.
+ * •	В классе Cat не должно быть переменных.
+ * •	В классе Cat не должно быть методов.
+ */
+
+public class Task_0212 {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        Cat cat3 = new Cat();
+        Cat cat4 = new Cat();
+        Cat cat5 = new Cat();
+        Cat cat6 = new Cat();
+        Cat cat7 = new Cat();
+        Cat cat8 = new Cat();
+        Cat cat9;
+        Cat cat10;
+    }
+
+    public static class Cat {
+
+    }
+}
