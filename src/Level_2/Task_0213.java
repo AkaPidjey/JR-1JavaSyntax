bc66d099e96b2d81d4eca128ba38be45e2f64409
AkaPidjey <pidjey@gmail.com>
2020-06-15 17:19:49 +0300

    * Title - Питомцам нужны люди # 0213.

---
+++ src/Level_2/Task_0213.java
@@ -0,0 +1,41 @@
+package Level_2;
+
+/**
+ * Title - Питомцам нужны люди # 0213.
+ * @task Создай объект типа Cat (кот), объект типа Dog (собака), объект типа Fish (рыбка) и объект типа Woman.
+ * Присвой каждому животному владельца (owner).
+ *
+ * Требования:
+ * •	Программа не должна выводить текст на экран.
+ * •	В методе main создай объекты типа Cat, Dog, Fish, Woman занеси их ссылки в переменные.
+ * •	В методе main присвойте каждому животному владельца Woman.
+ * •	Класс Cat, Dog, Fish должен содержать только одну переменную Woman owner.
+ * •	Класс Woman не должен содержать переменных.
+ */
+
+public class Task_0213 {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Fish fish = new Fish();
+        Woman woman = new Woman();
+        cat.owner = woman;
+        dog.owner = woman;
+        fish.owner = woman;
+    }
+
+    public static class Cat {
+        public Woman owner;
+    }
+
+    public static class Dog {
+        public Woman owner;
+    }
+
+    public static class Fish {
+        public Woman owner;
+    }
+
+    public static class Woman {
+    }
+}
