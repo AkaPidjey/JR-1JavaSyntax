97eedc8c8d56d914c779ec19891846fd8d23dcaf
AkaPidjey <pidjey@gmail.com>
2020-06-17 00:26:03 +0300

    * Title - Таблица умножения # 0314.

---
+++ src/Level_3/Task_0314.java
@@ -0,0 +1,61 @@
+package Level_3;
+
+/**
+ * Title - Таблица умножения # 0314.
+ * @task Выведи на экран таблицу умножения 10 на 10 в следующем виде:
+ * 1 2 3 4 ...
+ * 2 4 6 8 ...
+ * 3 6 9 12 ...
+ * 4 8 12 16 ...
+ * ...
+ *
+ * Требования:
+ * •	Программа должна выводить текст.
+ * •	Выведенный текст должен содержать 10 строк.
+ * •	Каждая выведенная строка должна содержать 10 чисел, разделенных пробелом.
+ * •	Выведенные числа должны быть таблицей умножения.
+ */
+
+public class Task_0314 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.print(1 * i + " ");
+        }
+        System.out.println();
+        for (int i = 1; i < 11; i++) {
+            System.out.print(2 * i + " ");
+        }
+        System.out.println();
+        for (int i = 1; i < 11; i++) {
+            System.out.print(3 * i + " ");
+        }
+        System.out.println();
+        for (int i = 1; i < 11; i++) {
+            System.out.print(4 * i + " ");
+        }
+        System.out.println();
+        for (int i = 1; i < 11; i++) {
+            System.out.print(5 * i + " ");
+        }
+        System.out.println();
+        for (int i = 1; i < 11; i++) {
+            System.out.print(6 * i + " ");
+        }
+        System.out.println();
+        for (int i = 1; i < 11; i++) {
+            System.out.print(7 * i + " ");
+        }
+        System.out.println();
+        for (int i = 1; i < 11; i++) {
+            System.out.print(8 * i + " ");
+        }
+        System.out.println();
+        for (int i = 1; i < 11; i++) {
+            System.out.print(9 * i + " ");
+        }
+        System.out.println();
+        for (int i = 1; i < 11; i++) {
+            System.out.print(10 * i + " ");
+        }
+    }
+}
