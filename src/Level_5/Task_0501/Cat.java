772985619163ed199d62a399d001810ac5335267
AkaPidjey <pidjey@gmail.com>
2020-06-18 22:24:15 +0300

    * Title - Создание кота # 0501.

---
+++ src/Level_5/Task_0501/Cat.java
@@ -0,0 +1,26 @@
+package Level_5.Task_0501;
+
+/**
+ * Title - Создание кота # 0501.
+ * @task Создать класс Cat. У кота должно быть имя (name, String), возраст (age, int),
+ * вес (weight, int), сила (strength, int).
+ *
+ * Требования:
+ * •	Программа не должна считывать данные с клавиатуры.
+ * •	У класса Cat должна быть переменная name с типом String.
+ * •	У класса Cat должна быть переменная age с типом int.
+ * •	У класса Cat должна быть переменная weight с типом int.
+ * •	У класса Cat должна быть переменная strength с типом int.
+ * •	Всего в классе должно быть 4 переменных.
+ */
+
+public class Cat {
+    String name;
+    int age;
+    int weight;
+    int strength;
+
+    public static void main(String[] args) {
+
+    }
+}
