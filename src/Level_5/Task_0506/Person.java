721b4b16c37a062ce1077b20deee6658a62ff3c6
AkaPidjey <pidjey@gmail.com>
2020-06-18 22:34:10 +0300

    * Title - Человечки # 0506.

---
+++ src/Level_5/Task_0506/Person.java
@@ -0,0 +1,25 @@
+package Level_5.Task_0506;
+
+/**
+ * Title - Человечки # 0506.
+ * @task Создать class Person. У человека должно быть имя String name, возраст int age,
+ * адрес String address, пол char sex.
+ *
+ * Требования:
+ * •	Программа не должна считывать данные с клавиатуры.
+ * •	У класса Person должна быть переменная name с типом String.
+ * •	У класса Person должна быть переменная age с типом int.
+ * •	У класса Person должна быть переменная address с типом String.
+ * •	У класса Person должна быть переменная sex с типом char.
+ * •	Всего в классе должно быть 4 переменных.
+ */
+
+public class Person {
+    String name;
+    int age;
+    String address;
+    char sex;
+
+    public static void main(String[] args) {
+    }
+}
