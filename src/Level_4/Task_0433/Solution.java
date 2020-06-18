3626cb4053f55a7c7b8a889442fcaf72da2ca474
AkaPidjey <pidjey@gmail.com>
2020-06-18 19:43:00 +0300

    * Title - Гадание на долларовый счет # 0433.

---
+++ src/Level_4/Task_0433/Solution.java
@@ -0,0 +1,91 @@
+package Level_4.Task_0433;
+
+/**
+ * Title - Гадание на долларовый счет # 0433.
+ * @task Вывести на экран квадрат из 10х10 букв S используя цикл while.
+ * Буквы в каждой строке не разделять.
+ *
+ * Пример вывода на экран:
+ * SSSSSSSSSS
+ * SSSSSSSSSS
+ * SSSSSSSSSS
+ * SSSSSSSSSS
+ * SSSSSSSSSS
+ * SSSSSSSSSS
+ * SSSSSSSSSS
+ * SSSSSSSSSS
+ * SSSSSSSSSS
+ * SSSSSSSSSS
+ *
+ * Требования:
+ * •	Программа не должна считывать текст c клавиатуры.
+ * •	Программа должна выводить текст на экран.
+ * •	Программа должна выводить квадрат из 10х10 букв S.
+ * •	В программе должен использоваться цикл while.
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        int s1 = 0;
+        int s2 = 0;
+        int s3 = 0;
+        int s4 = 0;
+        int s5 = 0;
+        int s6 = 0;
+        int s7 = 0;
+        int s8 = 0;
+        int s9 = 0;
+        int s10 = 0;
+
+        while (s1 < 10) {
+            System.out.print("S");
+            s1++;
+        }
+        System.out.println("");
+        while (s2 < 10) {
+            System.out.print("S");
+            s2++;
+        }
+        System.out.println("");
+        while (s3 < 10) {
+            System.out.print("S");
+            s3++;
+        }
+        System.out.println("");
+        while (s4 < 10) {
+            System.out.print("S");
+            s4++;
+        }
+        System.out.println("");
+        while (s5 < 10) {
+            System.out.print("S");
+            s5++;
+        }
+        System.out.println("");
+        while (s6 < 10) {
+            System.out.print("S");
+            s6++;
+        }
+        System.out.println("");
+        while (s7 < 10) {
+            System.out.print("S");
+            s7++;
+        }
+        System.out.println("");
+        while (s8 < 10) {
+            System.out.print("S");
+            s8++;
+        }
+        System.out.println("");
+        while (s9 < 10) {
+            System.out.print("S");
+            s9++;
+        }
+        System.out.println("");
+        while (s10 < 10) {
+            System.out.print("S");
+            s10++;
+        }
+        System.out.println("");
+    }
+}
