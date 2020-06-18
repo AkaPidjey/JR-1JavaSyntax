b85b42ce2e71b03a12542980e25e83f2e42e83d2
AkaPidjey <pidjey@gmail.com>
2020-06-18 19:51:24 +0300

    * Title Треугольник из восьмерок # 0437.

---
+++ src/Level_4/Task_0437/Solution.java
@@ -0,0 +1,59 @@
+package Level_4.Task_0437;
+
+/**
+ * Title Треугольник из восьмерок # 0437.
+ * @task Используя цикл for вывести на экран прямоугольный треугольник из восьмёрок со сторонами 10 и 10.
+ *
+ * Пример вывода на экран:
+ * 8
+ * 88
+ * 888
+ * 8888
+ * 88888
+ * 888888
+ * 8888888
+ * 88888888
+ * 888888888
+ * 8888888888
+ *
+ * Требования:
+ * •	Программа не должна считывать числа c клавиатуры.
+ * •	Программа должна выводить числа на экран.
+ * •	Программа должна выводить прямоугольный треугольник из восьмёрок со сторонами 10 и 10.
+ * •	В программе должен использоваться цикл for.
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int s1 = 0; s1 < 1; s1++)
+            System.out.print("8");
+        System.out.println("");
+        for (int s2 = 0; s2 < 2; s2++)
+            System.out.print("8");
+        System.out.println("");
+        for (int s3 = 0; s3 < 3; s3++)
+            System.out.print("8");
+        System.out.println("");
+        for (int s4 = 0; s4 < 4; s4++)
+            System.out.print("8");
+        System.out.println("");
+        for (int s5 = 0; s5 < 5; s5++)
+            System.out.print("8");
+        System.out.println("");
+        for (int s6 = 0; s6 < 6; s6++)
+            System.out.print("8");
+        System.out.println("");
+        for (int s7 = 0; s7 < 7; s7++)
+            System.out.print("8");
+        System.out.println("");
+        for (int s8 = 0; s8 < 8; s8++)
+            System.out.print("8");
+        System.out.println("");
+        for (int s9 = 0; s9 < 9; s9++)
+            System.out.print("8");
+        System.out.println("");
+        for (int s10 = 0; s10 < 10; s10++)
+            System.out.print("8");
+        System.out.println("");
+    }
+}
