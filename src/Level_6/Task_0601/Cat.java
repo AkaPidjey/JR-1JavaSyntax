d34b79f8e0e08a7f578be185ca44acedc228efc9
AkaPidjey <pidjey@gmail.com>
2020-06-19 21:56:45 +0300

    * Title - Метод finalize класса Cat # 0601.

---
+++ src/Level_6/Task_0601/Cat.java
@@ -0,0 +1,19 @@
+package Level_6.Task_0601;
+
+/**
+ * Title - Метод finalize класса Cat # 0601.
+ * @task В классе Cat создать метод protected void finalize() throws Throwable.
+ *
+ * Требования:
+ * •	В классе Cat должен быть метод с именем finalize.
+ * •	Метод finalize ничего не должен возвращать (void).
+ * •	Метод finalize должен быть protected.
+ * •	Метод finalize может кидать исключение Throwable, укажи это при объявлении метода.
+ */
+
+public class Cat {
+    protected void finalize() throws Throwable {}
+
+    public static void main(String[] args) {
+    }
+}
