df3178eeedc902748caba70d50b02bb9e87d958f
AkaPidjey <pidjey@gmail.com>
2020-06-19 22:30:55 +0300

    * Title - Три статические переменные name # 0619.

---
+++ src/Level_6/Task_0619/Solution.java
@@ -0,0 +1,27 @@
+package Level_6.Task_0619;
+
+/**
+ * Title - Три статические переменные name # 0619.
+ * @task Добавь 3 public статических переменных: String Solution.name, String Cat.name, String Dog.name.
+ *
+ * Требования:
+ * •	В класс Solution добавь public статическую переменную name типа String.
+ * •	В класс Cat добавь public статическую переменную name типа String.
+ * •	В класс Dog добавь public статическую переменную name типа String.
+ * •	В каждом классе должна быть своя переменная name.
+ */
+
+public class Solution {
+    public static String name;
+
+    public static class Cat {
+        public static String name;
+    }
+
+    public static class Dog {
+        public static String name;
+    }
+
+    public static void main(String[] args) {
+    }
+}
