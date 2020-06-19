537d4fb46a3bad801519149f165f8d3cdcf79d37
AkaPidjey <pidjey@gmail.com>
2020-06-19 22:25:03 +0300

    * Title - Феншуй и статики # 0615.

---
+++ src/Level_6/Task_0615/Solution.java
@@ -0,0 +1,21 @@
+package Level_6.Task_0615;
+
+/**
+ * Title - Феншуй и статики # 0615.
+ * @task Переставь один модификатор static, чтобы пример скомпилировался.
+ *
+ * Требования:
+ * •	Метод main менять нельзя.
+ * •	Добавь модификатор static в нужное место.
+ * •	Убери лишний модификатор static.
+ * •	В программе должно быть только 2 модификатора static.
+ */
+
+public class Solution {
+    public static int A = 5;
+    public int B = 2;
+    public int C = A * B;
+    public static void main(String[] args) {
+        A = 15;
+    }
+}
